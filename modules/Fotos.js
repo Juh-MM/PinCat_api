4e8dff3e284316661f39fe5b5332830f2ebfacfd
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-03-15 19:33:01 -0300

    Adicionar as fotos

--- modules/Fotos.js
+++ modules/Fotos.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const FotoSchema = new mongoose.Schema({
+    id: {type: Number, require: true, unique: true},
     title: {type: String, required: false},
     description: {type: String, required: false},
     image_url: {type: String, required: true}
