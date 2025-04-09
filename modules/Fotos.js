56d9b4f5dda5f4c5d7201e2ea3febe1485466e62
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-04-09 16:28:38 -0300

    Update Fotos.js

--- modules/Fotos.js
+++ modules/Fotos.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const FotoSchema = new mongoose.Schema({
     id: {type: Number, require: true, unique: true},
     title: {type: String, required: false},
-    url: {type: String, required: true},
-    createAt: {type: Date, default: Date.now},
+    url: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Foto', FotoSchema);
